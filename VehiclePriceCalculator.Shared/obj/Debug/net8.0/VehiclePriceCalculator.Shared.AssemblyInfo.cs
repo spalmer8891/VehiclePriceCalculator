5c6d5d09ae3acb54ff441795adeb26ce34939c0e
Wayne <sherwaynepalmer@hotmail.com>
2024-04-20 10:34:29 -0400

    commit to development

--- VehiclePriceCalculator.Shared/obj/Debug/net8.0/VehiclePriceCalculator.Shared.AssemblyInfo.cs
+++ VehiclePriceCalculator.Shared/obj/Debug/net8.0/VehiclePriceCalculator.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehiclePriceCalculator.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889c23f52609775e01bdc8cbc57940846cd6d66b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3971b0c501d98214419b266d012b21137a47e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehiclePriceCalculator.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehiclePriceCalculator.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
